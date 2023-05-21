a19d5f9016a6a70118e426c0b81ec686d26e3c02
ChouChouChen <134153447+ChouChouChen@users.noreply.github.com>
2023-05-21 15:25:28 +0800

    Update test.java

--- src/test.java
+++ src/test.java
@@ -4,5 +4,6 @@ public class test {
         System.out.println("22222222222222");
         System.out.println("33333333333333");
         System.out.println("push push");
+        System.out.println("pull pull");
     }
 }
